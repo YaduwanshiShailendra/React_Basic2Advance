fb256644adba9e2cbec8a5efe722971dfc1b9318
shailendra <shailendran70@gmail.com>
2020-08-24 18:37:39 +0530

    Create Simple Calculator App in React JS

--- 12_Simple_Calculator_App_in React_JS/src/index.js
+++ 12_Simple_Calculator_App_in React_JS/src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { add, mult, sub, div } from "./Calc";
+import App from "./App";
 
-ReactDOM.render(
-  <>
-    <ol>
-      <li> Sum of two no is {add(40, 4)}.</li>
-      <li> Subtraction of two no is {sub(40, 4)}.</li>
-      <li> Division of two no is {div(40, 4)}.</li>
-      <li> Multiplication of two no is {mult(40, 4)}.</li>
-    </ol>
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
