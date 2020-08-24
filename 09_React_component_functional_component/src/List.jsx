3b232f0338077f92be06f8322eec271e65def924
shailendra <shailendran70@gmail.com>
2020-08-24 16:38:27 +0530

    React component or Functional component

---
+++ 09_React_component_functional_component/src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function List() {
+  return (
+    <ol>
+      <li> Shailendra </li>
+      <li> Yadav </li>
+      <li> shaili </li>
+      <li> shanu </li>
+    </ol>
+  );
+}
+
+export default List;
